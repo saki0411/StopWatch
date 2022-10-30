08f630a2ed3f4de9faa9a2482ac5b366da1b6186
sakihosonuma <chipdale411@gmail.com>
2022-10-30 15:56:51 +0900

    Initial Commit

---
+++ StopWatch/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  StopWatch
+//
+//  Created by saki on 2022/10/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var label: UILabel!
+    
+    var count: Float = 0.0
+    var timer: Timer = Timer()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @objc func up(){
+        count = count + 0.01
+        label.text = String(format: "%.2f", count)
+    }
+    
+    @IBAction func start(){
+        if !timer.isValid {
+            timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(self.up), userInfo: nil, repeats: true)
+        }
+    }
+    
+    @IBAction func stop(){
+        if timer.isValid {
+            timer.invalidate()
+        }
+    }
+
+
+}
+
